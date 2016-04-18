ecdfb0d80d9574f6a2d14a76a5428bf2a70b343c
Jeson.ckon <ckontem@gmail.com>
2016-04-18 13:14:23 +0800

    upgrade

---
+++ src/CkRedis.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kontem
+ * Date: 16/4/6
+ * Time: 16:30.
+ */
+
+class CkRedis extends Handler
+{
+}
---
+++ src/Handler.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ */
+class Handler
+{
+    const ED = "\r\n";
+    protected $host;
+    protected $port;
+    protected $sock;
+    protected $config;
+    protected $timeout = 1;
+    protected $read_length = 1024;
+    public function __construct()
+    {
+        $this->_initialization(func_num_args(), func_get_args());
+        $this->_connect();
+    }
+    private function _initialization($num_args, $args)
+    {
+        
+    }
+    private function _connect()
+    {
+        $this->sock = fsockopen($this->host, $this->port, $errno, $errstr, $this->timeout);
+        if ($this->sock == false) {
+            #TODO
+        #echo "$errstr ($errno)<br />\n";
+        }
+
+        return true;
+    }
+}
