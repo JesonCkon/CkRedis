2fb3656cbbd5c538d7716e236cf35ba15a5dd971
Jeson.ckon <ckontem@gmail.com>
2016-04-18 20:46:25 +0800

    upgrade

---
+++ src/Application.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kontem
+ * Date: 16/4/6
+ * Time: 16:30.
+ */
+namespace CkRedis;
+
+class Application extends Container
+{
+    public $handler = null;
+    public $service_list = array(
+        'str' => RStrings::class,
+        'list' => RList::class,
+    );
+    private function _registerService()
+    {
+        foreach ($this->service_list as $key => $service_name) {
+            $this->_register(new $service_name(), $key, $service_name);
+        }
+    }
+    private function _register($service_obj, $key, $service_name)
+    {
+        $service_obj->register($this, $key, $service_name);
+
+        return $this;
+    }
+    public function __construct()
+    {
+        $args = func_get_args();
+        $this->handler = new Handler($args[0]);
+        $this->_registerService();
+    }
+}
